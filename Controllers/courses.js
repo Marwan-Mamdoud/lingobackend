76810b182a3ecc15b8fc5c6ef433596313a026c7
Marwan <marwanmamdouh169@gmail.com>
2024-09-27 15:27:18 +0300

    first

--- Controllers/courses.js
+++ Controllers/courses.js
@@ -21,8 +21,8 @@ export const CreateCourse = async (req, res, next) => {
 
 export const getCourses = async (req, res, next) => {
   try {
-    const courses = await Courses.find();
-    // if (!courses) throw new Error("Cant find courses");
+    // const courses = await Courses.find();
+    if (!courses) throw new Error("Cant find courses");
     res
       .status(200)
       .json({ messsage: "Done get courses successfully", courses });
